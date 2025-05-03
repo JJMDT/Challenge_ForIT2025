ec829932481a0179d7580b6c2a651681d99d8fe8
JJMDT <127325507+JJMDT@users.noreply.github.com>
2025-05-03 19:41:55 -0300

    se realizaron las rutas, modelo y controladores, y se implemento .gitignore y .env

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {createTask,deleteTask,getAllTasks,updateTask} = require('../controllers/taskController')
+const {createTask,deleteTask,getAllTasks,updateTask,getTask} = require('../controllers/taskController')
 
 router.get('/', getAllTasks)
 router.post('/', createTask)
+router.get('/:id', getTask)
 router.delete('/:id', deleteTask)
 router.put('/:id',updateTask)
 
